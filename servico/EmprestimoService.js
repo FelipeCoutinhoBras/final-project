85f157ac446899a3dc81f296b308c63053dcdce8
Felipe Bras Coutinho <felipebrascoutinho@gmail.com>
2024-09-06 22:49:03 -0300

    Sempre que um livro é emprestado, o seu estado será alterado como emprestado na tabela de livros

--- servico/EmprestimoService.js
+++ servico/EmprestimoService.js
@@ -1,12 +1,13 @@
 const { DataTypes, Op } = require("sequelize");
 const EmprestimoModel = require("../model/Emprestimo");
+const LivroModel = require("../model/Livro");
 
 module.exports = {
   // Lista todos os empréstimos
   list: async function (limit, offset) {
     const emprestimos = await EmprestimoModel.findAll({
       limit: limit,
-      offset: offset
+      offset: offset,
     });
     return emprestimos;
   },
@@ -19,12 +20,18 @@ module.exports = {
     LivroId,
     FuncionarioId
   ) {
+    // Atualiza o estado do livro para "emprestado"
+    await LivroModel.update(
+      { estado: "emprestado" },
+      { where: { id: LivroId } }
+    );
+
     const novoEmprestimo = await EmprestimoModel.create({
       data_emprestimo: data_emprestimo,
       data_devolucao: data_devolucao,
       cliente: cliente,
       LivroId: LivroId,
-      FuncionarioId: FuncionarioId
+      FuncionarioId: FuncionarioId,
     });
     return novoEmprestimo;
   },
@@ -43,12 +50,20 @@ module.exports = {
       return null; // Retorna null se o empréstimo não for encontrado
     }
 
+    // Atualiza o estado do livro para "emprestado"
+    await LivroModel.update(
+      { estado: "emprestado" },
+      { where: { id: LivroId } }
+    );
+
     await EmprestimoModel.update(
-      { data_emprestimo: data_emprestimo,
+      {
+        data_emprestimo: data_emprestimo,
         data_devolucao: data_devolucao,
         cliente: cliente,
         LivroId: LivroId,
-        FuncionarioId: FuncionarioId},
+        FuncionarioId: FuncionarioId,
+      },
       { where: { id } }
     );
 
